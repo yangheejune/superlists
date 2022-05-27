58aa4b53656088e2f1a402d1d57a9e4e6cdd5a8f
yangheejune <yangheejune@gmail.com>
2022-05-27 11:22:02 +0900

    레이아웃 개선을 위한 부트스트랩 사용

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,10 +1,11 @@
-from django.test import LiveServerTestCase
+#from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import unittest
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     
     def setUp(self):
         self.browser = webdriver.Firefox()
